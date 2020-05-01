4ea3950ed29d689ca51b9188765708dc382f5420
jj1lis <rinta.nambuline205@gmail.com>
2020-05-01 12:22:17 +0900

    [update]

--- templates/maint_template.d
+++ templates/maint_template.d
@@ -19,7 +19,9 @@ T[] scanarr(T=long)(){
 }
 
 alias Queue=DList;
-auto enq(T)(ref Queue!T q,T e){q.insertBack(e);}
+auto enq(T)(ref Queue!T q,T e){
+    q.insertBack(e);
+}
 T deq(T)(ref Queue!T q){
     auto e=q.front;
     q.removeFront;
@@ -27,7 +29,9 @@ T deq(T)(ref Queue!T q){
 }
 
 alias Stack=SList;
-auto push(T)(ref Stack!T s,T e){s.insert(e);}
+auto push(T)(ref Stack!T s,T e){
+    s.insert(e);
+}
 T pop(T)(ref Stack!T s){
     auto e=s.front;
     s.removeFront;
@@ -36,15 +40,18 @@ T pop(T)(ref Stack!T s){
 
 
 struct UnionFind(T){
-    T[T]u;
+    T[T] u;
     ulong[T] rank;
     @property{
+
         bool inc(T e){
             return e in u;
         }
+
         auto size(){
             return u.keys.length;
         }
+
         auto dup(){
             T[] child=u.keys;
             T[] parent=u.values;
@@ -54,22 +61,28 @@ struct UnionFind(T){
             return res;
         }
     }
+
     this(T e){
         e.add;
     }
+
     this(T[] es){
         es.each!(e=>e.add);
     }
+
     auto add(T a,T b=a){
         assert(b.inc);
         u[a]=a;rank[a];
         if(a!=b)
-        unite(a,b);
+            unite(a,b);
     }
+
     auto find(T e){
-        if(u[e]==e)return e;
+        if(u[e]==e)
+            return e;
         return u[e]=find(u[e]);
     }
+
     auto same(T a,T b){
         return a.find==b.find;
     }
@@ -113,7 +126,18 @@ struct PriorityQueue(T,alias less="a<b"){
         heap=heapify!(less,T[])(e);
     }   
 }
+
+auto cumulativeSum(T)(T[] arr){
+    T[] tmp;
+    (arr.length).iota.each!(i=>tmp~=i==0?arr[0]:tmp[i-1]+arr[i]);
+    return tmp;
+}
+
+//TODO
+//auto cumulativeXOR(T)()
+
 //END OF TEMPLATE
 
 void main(){
+    [1,2,3,4,5,6,7,8,9,10].cumulativeSum.writeln;
 }
